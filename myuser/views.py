be0e99c790c9291d1b83d7a277c01ba717e6951d
rapospectre <rapospectre@163.com>
2016-05-14 11:57:36 +0800

    fix invite bind

--- myuser/views.py
+++ myuser/views.py
@@ -101,15 +101,15 @@ class UserRegisterView(CheckSecurityMixin, StatusWrapMixin, JsonResponseMixin, C
         if code.exists():
             code = code[0]
             if not code.use:
-                code.use = True
-                code.belong = self.object
-                code.save()
                 super(UserRegisterView, self).form_valid(form)
                 self.create_extend()
                 self.token = self.create_token()
                 self.object.token = self.token
                 self.object.set_password(form.cleaned_data.get('password'))
                 self.object.save()
+                code.use = True
+                code.belong = self.object
+                code.save()
                 return self.render_to_response(self.object)
             else:
                 self.message = '邀请码已使用'
