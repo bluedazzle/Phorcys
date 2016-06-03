a7a06478f71627b05486f4149f8aa937f81fde9f
rapospectre <rapospectre@163.com>
2016-06-03 15:47:20 +0800

    add code

--- myuser/views.py
+++ myuser/views.py
@@ -185,9 +185,9 @@ class UserThirdRegisterView(CheckSecurityMixin, StatusWrapMixin, JsonResponseMix
         self.object.token = self.token
         self.object.set_password(form.cleaned_data.get('password'))
         self.object.save()
-        code.use = True
-        code.belong = self.object
-        code.save()
+        # code.use = True
+        # code.belong = self.object
+        # code.save()
         return self.render_to_response(self.object)
         #     else:
         #         self.message = '邀请码已使用'
